884b2fb0f6397212ec5b1f54023cb0ff782bf16a
badenk4271 <ayavarav@gmail.com>
2017-10-19 19:20:58 -0300

    nombre de controles
    se le asigno nuevo nombre a los controles

--- application/views/formulario.php
+++ application/views/formulario.php
@@ -21,9 +21,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </ul>
     
     <form class="form">
-      <input type="text" placeholder="primer nombre" class="textbox" />
-      <input type="text" placeholder="Apellido" class="textbox" />
-      <input type="text" placeholder="Direccion de Email" class="textbox" />
+      <input type="text" placeholder="primer nombre" class="textbox" name="nombres2" />
+      <input type="text" placeholder="Apellido" class="textbox" name="apellidos2" />
+      <input type="text" placeholder="Direccion de Email" class="textbox" name="correo" />
       <input type="button" value="Next" class="button" />
     </form>
   </div>
